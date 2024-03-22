917bcb0ca6b9cbf00e9e60d5fe38dd5c34856379
Philipp Maier <pmaier@sysmocom.de>
2024-03-22 16:35:48 +0100

    esipa: add info page to confirm that the eIM is reachable

---
+++ onomondo_eim/src/esipa_infopage.erl
@@ -0,0 +1,13 @@
+% Author: Philipp Maier <pmaier@sysmocom.de> / sysmocom - s.f.m.c. GmbH
+%
+% This is testpage to allow for an easy conformation that the eIM is reachable.
+
+-module(esipa_infopage).
+
+-export([init/2]).
+
+init(Req0, State) ->
+    Req = cowboy_req:reply(200, #{
+				  <<"content-type">> => <<"text/plain">>
+				 }, <<"eIM working!">>, Req0),
+    {ok, Req, State}.
