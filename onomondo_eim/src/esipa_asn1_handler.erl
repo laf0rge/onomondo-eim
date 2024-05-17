0dc3cb8c0f550f93bfe2d7819a91ad787164a7c9
Philipp Maier <pmaier@sysmocom.de>
2024-05-17 09:57:52 +0200

    esipa_asn1_handler: move PSMO conversion code to separate file
    The code that converts an order to an euiccPackage with a psmoList
    tends to grow larger, let's put it in a dedicated file.
    
    Related: SYS#6720

--- onomondo_eim/src/esipa_asn1_handler.erl
+++ onomondo_eim/src/esipa_asn1_handler.erl
@@ -198,33 +198,7 @@ handle_asn1(Req0, _State, {getEimPackageRequest, EsipaReq}) ->
 			%{[{<<"activationCode">>, ActivationCode}]} = Order,
 			{profileDownloadTriggerRequest, #{profileDownloadData => {activationCode, ActivationCode}}};
 		    {psmo, Order} ->
-                        % Convert Order to PSMO list
-			{[{<<"psmo">>, PsmoOrderList}]} = Order,
-			Order2Psmo = fun(PsmoOrder) ->
-					     case PsmoOrder of
-						 {[{<<"enable">>, {[{<<"iccid">>,Iccid}, {<<"rollback">>,true}]}}]} ->
-						     {enable, #{iccid => utils:hex_to_binary(Iccid), rollbackFlag => null}};
-						 {[{<<"enable">>, {[{<<"iccid">>,Iccid}, {<<"rollback">>,false}]}}]} ->
-						     {enable, #{iccid => utils:hex_to_binary(Iccid)}};
-						 {[{<<"disable">>, {[{<<"iccid">>,Iccid}]}}]} ->
-						     {disable, #{iccid => utils:hex_to_binary(Iccid)}};
-						 {[{<<"delete">>, {[{<<"iccid">>,Iccid}]}}]} ->
-						     {delete, #{iccid => utils:hex_to_binary(Iccid)}}
-						 % TODO: when nothing matches, we get a crash report and nothing else
-						 % happens. We should instead ensure that an undefinedError is returned
-						 % to the ipad and the work is finished with a failure status.
-					     end
-				     end,
-			PsmoList = [Order2Psmo(O) || O <- PsmoOrderList ],
-
-			% Format Esipa message
-			EuiccPackage = {psmoList, PsmoList},
-			{ok, EimId} = application:get_env(onomondo_eim, eim_id),
-			EuiccPackageSigned = #{eimId => list_to_binary(EimId),
-					       eidValue => EidValue,
-					       counterValue => 0, % TODO: pick a suitable value (how?)
-					       transactionId => <<1,2,3,4>>, %TODO: generate a random transaction id (and store it?)
-					       euiccPackage => EuiccPackage},
+			EuiccPackageSigned = esipa_euiccPackage:order_to_euiccPackageSigned(Order, EidValue),
 			{euiccPackageRequest, #{euiccPackageSigned => EuiccPackageSigned, eimSignature => <<0,0,0>>}};
 		    none ->
 			{eimPackageError, noEimPackageAvailable};
