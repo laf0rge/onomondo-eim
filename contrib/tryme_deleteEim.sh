4f2c1a47b74d13409001fc2f8b55cc42e4eefb81
Philipp Maier <pmaier@sysmocom.de>
2024-06-11 17:54:47 +0200

    contrib: add new script to try out the deleteEim eCO
    Related: SYS#6564

---
+++ contrib/tryme_deleteEim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+. ./tryme.cfg
+EIM_ID="eIM"
+JSON='{ "eidValue" : "'$EID'", "order" : { "eco" : [ { "deleteEim" : { "eimId" : "'$EIM_ID'" } } ] } }'
+RC=`./restop.py -c -f eco -j "$JSON"`
+echo $RC
+
+echo "---------------------------------------8<---------------------------------------"
+RESOURCE_ID=`echo $RC | cut -d '/' -f 6`
+echo "ResourceId =" $RESOURCE_ID
+./tryme_lookup.sh eco $RESOURCE_ID
